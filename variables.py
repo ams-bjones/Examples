09ef7334e97bf7285ed71a71b800a4dae3b05b3b
Brian Jones <bjones@ashmanorschool.com>
2014-03-05 13:02:14 +0000

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+#A variable can store things;
+
+variable = 2
+#This variable is storing the number 2 as an Integer
+
+anotherVariable = 2.0
+#this variable is storing the numebr 2 as a Float.
+
+yetAnotherVariable = '2'
+#this variable is storing the number 2 as a string
+
+print(variable)
+#this line will display the number 2 to the screeen.  The numeber was stored in Line 3
+
+print (anotherVariable) 
+#This line will display the number 2.0 to the screen.  The number 2.0 was stored in Line 6
+
+print(yetAnotherVariable)
+#This line will display the character 2 to the screen, this was stored in Line 9
