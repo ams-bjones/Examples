10e2f6bd8cb4b08397c3cde5a586b659bddaa0dd
Brian Jones <bjones@ashmanorschool.com>
2014-03-05 13:06:27 +0000

    Update variables.py

--- variables.py
+++ variables.py
@@ -4,10 +4,10 @@ variable = 2
 #This variable is storing the number 2 as an Integer
 
 anotherVariable = 2.0
-#this variable is storing the numebr 2 as a Float.
+#this variable is storing the number 2 as a Float.
 
 yetAnotherVariable = '2'
-#this variable is storing the number 2 as a string
+#this variable is storing the number 2 as a String
 
 print(variable)
 #this line will display the number 2 to the screeen.  The numeber was stored in Line 3
@@ -17,3 +17,6 @@ print (anotherVariable)
 
 print(yetAnotherVariable)
 #This line will display the character 2 to the screen, this was stored in Line 9
+
+CONSTANT = 2
+#variables written in all caps are constants.  These chould not be changed.
